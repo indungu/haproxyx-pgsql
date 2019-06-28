70beeb891849dcd7722330550359f6507c174a7e
indungu <isaac.ndungu@andela.com>
2019-06-28 21:12:44 +0300

    fix slave image provisioner

--- images/slave-db/provisioner.sh
+++ images/slave-db/provisioner.sh
@@ -13,6 +13,6 @@ sudo cp /tmp/configs/postgresql.conf /etc/postgresql/9.5/main/postgresql.conf
 echo "Setup replication"
 sudo systemctl stop postgresql
 sudo mv ../../var/lib/postgresql/9.5/main ../../var/lib/postgresql/9.5/main_old
-sudo -u postgres pg_basebackup -h 10.132.0.76 -D /var/lib/postgresql/9.5/main -U replica -v -xlog-method=stream
+sudo -u postgres pg_basebackup -h 10.132.0.46 -D /var/lib/postgresql/9.5/main -U replica -v -xlog-method=stream
 sudo cp /tmp/configs/recovery.conf /var/lib/postgresql/9.5/main/recovery.conf
 sudo systemctl start postgresql
